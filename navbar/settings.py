5e650f4a105449250eff51375ddc6413fb876107
Aioramu <king-gun@mail.ru>
2021-03-27 15:12:37 +0300

    bugsfix

--- navbar/settings.py
+++ navbar/settings.py
@@ -76,14 +76,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'navbar.wsgi.application'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-    'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
